69facc353754144b86d4113d6ceeacdfbd58e341
Harry Tang <harry.duong@parkman.io>
2020-10-17 20:27:24 +0300

    fix: only disable email from script

--- scripts/user_create_post.sh
+++ scripts/user_create_post.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 CURUNIXTIME=`date +"%s"`
-echo "${username}: ${CURUNIXTIME}" >> /etc/virtual/blacklist_usernames
+echo "${username}: ${CURUNIXTIME}" >> /etc/virtual/blacklist_script_usernames
