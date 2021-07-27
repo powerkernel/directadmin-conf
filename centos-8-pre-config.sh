146104619dd4961d8e05b8c3d5ced1cf4ce3f7ee
Harry Tang <harry@powerkernel.com>
2021-07-27 21:49:33 +0300

    Update centos-8-pre-config.sh

--- centos-8-pre-config.sh
+++ centos-8-pre-config.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-dnf install dnf-automatic
+dnf install dnf-automatic -y
 sed -i "s/apply_updates = no/apply_updates = yes/g" /etc/dnf/automatic.conf
 systemctl enable --now dnf-automatic.timer
 sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"rootflags=uquota,pquota /g" /etc/default/grub
