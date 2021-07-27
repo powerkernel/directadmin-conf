95f8dd3d5d127b9e47db9580b4b038bdfb60845c
Harry Tang <harry@powerkernel.com>
2021-07-27 21:41:15 +0300

    Update and rename centos-8-auto-upgrade.sh to centos-8-pre-config.sh

--- centos-8-auto-upgrade.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-dnf install dnf-automatic
-sed -i "s/apply_updates = no/apply_updates = yes/g" /etc/dnf/automatic.conf
-systemctl enable --now dnf-automatic.timer
---
+++ centos-8-pre-config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+dnf install dnf-automatic
+sed -i "s/apply_updates = no/apply_updates = yes/g" /etc/dnf/automatic.conf
+systemctl enable --now dnf-automatic.timer
+sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"rootflags=uquota,pquota /g" /etc/default/grub
+cp /boot/grub2/grub.cfg /boot/grub2/grub.cfg.orig
+grub2-mkconfig -o /boot/grub2/grub.cfg
+reboot
