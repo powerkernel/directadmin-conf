843fe811981fc53437570ad345111750e5ab3b83
Harry Tang <harry@powerkernel.com>
2021-07-28 17:26:58 +0300

    Update centos-8-pre-config.sh

--- centos-8-pre-config.sh
+++ centos-8-pre-config.sh
@@ -5,4 +5,7 @@ systemctl enable --now dnf-automatic.timer
 sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"rootflags=uquota,pquota /g" /etc/default/grub
 cp /boot/grub2/grub.cfg /boot/grub2/grub.cfg.orig
 grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+sudo systemctl mask --now firewalld
 reboot
