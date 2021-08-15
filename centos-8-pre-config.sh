a68c52a99f394fd6383a94697345a2d2da7f0125
Harry Tang <harry@powerkernel.com>
2021-08-15 13:51:49 +0300

    Update centos-8-pre-config.sh

--- centos-8-pre-config.sh
+++ centos-8-pre-config.sh
@@ -5,4 +5,6 @@ systemctl enable --now dnf-automatic.timer
 sed -i "s/GRUB_CMDLINE_LINUX=\"/GRUB_CMDLINE_LINUX=\"rootflags=uquota,pquota /g" /etc/default/grub
 cp /boot/grub2/grub.cfg /boot/grub2/grub.cfg.orig
 grub2-mkconfig -o /boot/grub2/grub.cfg
+dnf install perl-IO-Socket-INET6.noarch -y
+dnf install perl-LWP-Protocol-https -y
 reboot
